702a555ffd4c980543524e320fae87cb87cc9238
haleydek <hcd09@live.com>
2019-07-26 20:52:59 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+funny = Category.create!(name: "Funny")
+sad = Category.create!(name: "Sad")
+happy = Category.create!(name: "Happy")
+
+happy.posts.create!(title: "Happy Post :)", content: "Smiling ALLLLLLLLLLLL day long!")
+happy.posts.create!(title: "Graduation", content: "I'm so happy I graduated!")
